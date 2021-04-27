55ccbb65d9c2282654d163d1f7b048550b3d4817
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-27 17:13:35 +0200

    Buggatissimo

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            mailArray: []
+        },
+        methods: {
+        },
+        mounted() {
+            for(let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const result = response.data.response;
+                    this.mailArray.push(result);
+                    console.log(this.mailArray);
+                });
+            }
+        }
+    }
+)

f4735841e3334ac5e1cc32933dd627a8ccf60151
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-27 17:33:06 +0200

    Finito esercizio

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ var app = new Vue (
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
+                    console.log(response);
                     const result = response.data.response;
                     this.mailArray.push(result);
                     console.log(this.mailArray);
